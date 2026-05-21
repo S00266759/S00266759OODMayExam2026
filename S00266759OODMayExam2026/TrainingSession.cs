3cc4557e8bd92fc10fa11571347839e212a7d2d2
Hannah Rodtmann <S00266759@atu.ie>
2026-05-21 13:22:01 +0100

    Q.1 (d) complete - TrainingSession class and properties implemented

---
+++ S00266759OODMayExam2026/TrainingSession.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S00266759OODMayExam2026
+{
+    public class TrainingSession
+    {
+        public int SessionID { get; set; }
+        public DateTime SessionDate { get; set; }
+        public string SessionType { get; set; }
+        public int DurationMinutes { get; set; }
+        public string CoachNotes { get; set; }
+
+    }
+}
